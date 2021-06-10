1784367563fe08d6135ea8c53e7d2932c571f6d1
Mimoun Rahmouni <mimoun06270@hotmail.fr>
2021-06-10 15:01:19 -0400

    initialisation test react

---
+++ src/components/App.js
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import Banner from './Banner';
+
+function App() {
+const [email, setEmail] = useState('')
+const [password, setPassword] = useState('')
+const [stop, loading] = useState(false)
+console.log(email)
+
+  useEffect(async () => {
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email: email, password: password})
+  };
+  fetch('http://localhost:3001/api/employes/login', requestOptions)
+      .then(response => response.json())
+      .then(data => console.log(data));
+      }, [stop])
+  return (
+    <div>
+      <Banner/>
+      <form>
+  <label>
+    email:
+    <input type="text" name="email" value={email}
+                onChange={(e) => setEmail(e.target.value)}/>
+    password:
+    <input type="text" name="password" value={password}
+                onChange={(e) => setPassword(e.target.value)}/>
+  </label>
+</form>
+  <button onClick ={() => stop === true ? loading(false) : loading(true)}>New Joke</button>
+      
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Banner.js
@@ -0,0 +1,14 @@
+import logo from '../assets/icon-left-font-monochrome-white.png'
+import '../sass/app.css'
+
+const Banner = () => {
+    return (
+        <header className='banner'>
+            <img className="banner_logo" src={logo} alt='Logo groupomania'/>
+            
+            
+        </header>
+    )
+}
+
+export default Banner
