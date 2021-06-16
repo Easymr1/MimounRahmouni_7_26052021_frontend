8daf214b920c7c94328f398804de4211e05b27f6
Mimoun Rahmouni <mimoun06270@hotmail.fr>
2021-06-16 14:42:48 -0400

    ajout du commentaire sur les publications

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,6 +5,7 @@ import axios from 'axios';
 
  const Home = () => {
     const [getPublication, setGetPublication] = useState([])
+
     const [refresh, setRefresh] = useState(false)
     console.log(refresh)
     useEffect(() => {
@@ -12,6 +13,10 @@ import axios from 'axios';
         .then(res => setGetPublication(res.data.results), setRefresh(false))
         .catch(err => console.error(err))
     }, [refresh])
+
+    
+
+
     return (
         <section className='section'>
             <Publier />
@@ -20,9 +25,29 @@ import axios from 'axios';
                 <article key={publication.id}>
                 <h3>{publication.titre}</h3>
                 <p>{publication.texte}</p>
+                <Commentaire id={publication.id} refresh={refresh}/>
             </article>
                 )}
         </section>
+    )
+}
+
+function Commentaire ({id, refresh}) { 
+    const [getCommentaires, setGetCommentaires] = useState([])
+useEffect(() => {
+        axios.get(`http://localhost:3001/api/commentaire/${id}`)
+        .then(res => setGetCommentaires(res.data.results))
+        .catch(err => console.error(err))
+    }, [refresh])
+
+    return (
+        <div>
+            {getCommentaires.map(commentaire => 
+                <div key={commentaire.id}>
+                    <h5>{commentaire.texte}</h5>
+                </div>
+            )}
+        </div>
     )
 }
 
